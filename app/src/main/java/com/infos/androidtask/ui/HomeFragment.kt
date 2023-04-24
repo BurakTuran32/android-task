5a081c3711bdaa4d0bd6f7a039265b3e4e655679
burakTuran <buraktt00@gmail.com>
2023-04-25 01:27:15 +0300

    [Android Task][Created base and architectural]

---
+++ app/src/main/java/com/infos/androidtask/ui/HomeFragment.kt
@@ -0,0 +1,19 @@
+package com.infos.androidtask.ui
+
+import android.os.Bundle
+import android.view.View
+import com.infos.androidtask.base.BaseFragment
+import com.infos.androidtask.databinding.FragmentHomeBinding
+
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+
+}
